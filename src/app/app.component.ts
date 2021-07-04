1900721be2c60aa68bf248b41c17ca3e4a2b7dc9
wichaisw <wichai.sw19@gmail.com>
2021-07-04 20:26:37 +0700

    add string interpolation, property binding,  two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-to-do-list';
+  title: string = 'Basic Angular Form';
+  content1: any = '';
+  content2: any = '';
+
+  onContentChange(event: Event) {
+    this.content1 = (<HTMLInputElement>event.target).value;
+  };
+
+
 }
+
+
