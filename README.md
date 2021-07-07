4eda74316928797b4e86d86bbbcfdda180454b53
wichaisw <wichai.sw19@gmail.com>
2021-07-07 20:13:15 +0700

    add @ViewCHild dom accessing example

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 Basic Decorator
 * @Input
 * @Output
-* @Viewchild (optional)
+* @Viewchild (DOM accessing)
 
 Basic Directive
 * ngIf
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title: string = 'Basic Angular Form';
   content1: any = '';
   content2: any = '';
+  content3: string = '';
+  @ViewChild('content3Input') content3Input: ElementRef = new ElementRef<string>('');
+
 
   onContentChange(event: Event) {
     this.content1 = (<HTMLInputElement>event.target).value;
+  };
+
+  onContentChange3() {
+    this.content3 = this.content3Input.nativeElement.value;
   };
 
 
